a6ec55763b52a9748b68c01877c0ebc2d7a84890
shogunsea <shogunsea@foxmail.com>
2014-02-09 00:06:45 -0500

    try offical solution for http collect, add bl module

--- http_colletct.js
+++ http_colletct.js
@@ -73,21 +73,21 @@
 //  » For a list of those who contributed to learnyounode, run:
 //    `learnyounode credits`.
 
-var http = require('http');
+// var http = require('http');
 
-http.get(process.argv[2], function(res) {
-  var message = '';
+// http.get(process.argv[2], function(res) {
+//   var message = '';
 
-  res.on('data', function(chunk) {
-    message += String(chunk);
-  });
+//   res.on('data', function(chunk) {
+//     message += String(chunk);
+//   });
 
-  res.on('end', function() {
-    console.log(message.length);
-    console.log(message);
-  });
+//   res.on('end', function() {
+//     console.log(message.length);
+//     console.log(message);
+//   });
 
-});
+// });
 
 // Verifying "HTTP COLLECT"...
 
@@ -109,18 +109,19 @@ http.get(process.argv[2], function(res) {
 // -----------------------------------------------------------------
 
 
-//       var http = require('http')
-//       var bl = require('bl')
+      var http = require('http')
+      var bl = require('bl')
 
-//       http.get(process.argv[2], function (response) {
-//         response.pipe(bl(function (err, data) {
-//           if (err)
-//             return console.error(data)
-//           data = data.toString()
-//           console.log(data.length)
-//           console.log(data)
-//         }))
-//       })
+      http.get(process.argv[2], function (response) {
+      	console.log(response.pipe);
+        response.pipe(bl(function (err, data) {
+          if (err)
+            return console.error(data)
+          data = data.toString()
+          console.log(data.length)
+          console.log(data)
+        }))
+      })
 
 
 // -----------------------------------------------------------------
